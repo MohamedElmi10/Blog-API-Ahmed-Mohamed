bdbfd896f7cc8b919873773c020009f828bfb87a
Ahmed Idiris <aamedidris1999@gmail.com>
2023-12-08 17:41:46 +0100

    json

--- js/posts.js
+++ js/posts.js
@@ -1,6 +1,7 @@
 async function fetchAllBlog(){
   try{
-    const response = await fetch('https://blog-api-assignment.up.railway.app/posts')
+    const response = await fetch('https://blog-api-assignment.up.railway.app/posts');
+    const blogs = await response.json();
   }
   catch (error){
 console.log(error);
