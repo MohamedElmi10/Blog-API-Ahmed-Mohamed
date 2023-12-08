ea644bc5a0832b14ccb4eae9708e8277b64c6118
Ahmed Idiris <aamedidris1999@gmail.com>
2023-12-08 17:40:42 +0100

    catch error

--- js/posts.js
+++ js/posts.js
@@ -2,7 +2,7 @@ async function fetchAllBlog(){
   try{
     const response = await fetch('https://blog-api-assignment.up.railway.app/posts')
   }
-  catch{
-
+  catch (error){
+console.log(error);
   }
 }
