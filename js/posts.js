7bffab335c873f7f5adc09cd4f027362567bcc36
Ahmed Idiris <aamedidris1999@gmail.com>
2023-12-08 18:13:03 +0100

    some changes

--- js/posts.js
+++ js/posts.js
@@ -1,19 +1,20 @@
 async function fetchAllBlog(){
-  try{
+  try {
     const response = await fetch('https://blog-api-assignment.up.railway.app/posts');
     const blogs = await response.json();
 
     let blogsListHTML = "";
-    for(let blog of blogs){
-      const truncatedContent = blog.content.substring(0,100);
+    for(let blog of blogs)  {
+      const truncatedContent = blog.content.substring(0, 100);
+      const readMoreLink = `<a href="post.html?id=${blog._id}" class="read-more">Read more..</a>`;
       blogsListHTML += `
       
-       <div id="blog-post">
+     
       <div class="blog-post">
           <h2><span class="title">${blog.title}</span></h2>
-          <p><span class="author" class="date">${blog.author} | ${blog.date}</span></p>
+          <p><span class="author date">${blog.author} | ${blog.date}</span></p>
           <p><b>tags:</b><span class="tags">${blog.tags.join(', ')}</span></p>
-          <p><span class="content">${truncatedContent}</span></p>
+          <p><span class="content">${truncatedContent}</span>${readMoreLink}</p>
           </div>
           `;
     } 
@@ -21,7 +22,7 @@ async function fetchAllBlog(){
   }
  
   catch (error){
-console.log(error);
+console.error(error);
   }
 }
 fetchAllBlog();
