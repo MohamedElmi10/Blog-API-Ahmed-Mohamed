6a94528ce4490985981ff09534e4b6103abcca4c
Ahmed Idiris <aamedidris1999@gmail.com>
2023-12-08 17:47:00 +0100

    struktur för hur varje bloggpost

--- js/posts.js
+++ js/posts.js
@@ -5,7 +5,16 @@ async function fetchAllBlog(){
 
     let blogsListHTML = "";
     for(let blog of blogs){
+      blogsListHTML += `
       
+       <div id="blog-post">
+      <div class="blog-post">
+          <h2><span class="title"></span></h2>
+          <p><span class="author" class="date"></span></p>
+          <p><b>tags:</b><span class="tags"></span></p>
+          <p><span class="content"></span></p>
+          </div>
+          `
     }
   }
   catch (error){
