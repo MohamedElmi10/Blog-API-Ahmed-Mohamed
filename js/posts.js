66cbd593a6668b20e1a5f7de81a6ae807d363062
Ahmed Idiris <aamedidris1999@gmail.com>
2023-12-11 09:51:46 +0100

    ändra ett fel

--- js/posts.js
+++ js/posts.js
@@ -18,7 +18,7 @@ async function fetchAllBlog(){
           </div>
           `;
     } 
-    document.getElementById('blog-post').innerHTML = blogsListHTML;
+    document.getElementById('blog-posts').innerHTML = blogsListHTML;
   }
  
   catch (error){
