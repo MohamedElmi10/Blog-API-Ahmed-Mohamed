27a8f3b0c749314d804547c7d73df201a3be1a86
Ahmed Idiris <aamedidris1999@gmail.com>
2023-12-11 10:04:23 +0100

    function try catch

--- js/post.js
+++ js/post.js
@@ -1,3 +1,12 @@
 const urlParams = new URLSearchParams(window.location.search);
 const postId = urlParams.get('id');
 
+
+async function fetchPostDetails() {
+  try{
+
+  }
+  catch (error) {
+console.error('Error fetching post details:', error.message)
+  }
+}
