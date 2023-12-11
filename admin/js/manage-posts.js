238efe132e4f7a7318058a3bf070d1d9b178d4cd
Mohamed Elmi <mohamed.elmiefc@gmail.com>
2023-12-11 11:33:10 +0100

    made somechanges

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -15,16 +15,38 @@ async function fetchAllPosts() {
             <td>${post.tags}</td>
             <td>${postDate}</td>
             <td>
-                <a href="">update</a><br>
-                <a href="">Delete</a>
+                <a href="update-post.html" id="updateLink">update</a><br>
+                <a href="#" data-id="${post._id}" class="delete-links">Delete</a>
             </td>
             </tr>`
-            document.getElementById("tbody").innerHTML = htmlForThePost;
+
 
 
         }
+        document.getElementById("tbody").innerHTML = htmlForThePost;
     }
     catch (error) {
         console.log(error)
     }
+}
+let deleteLink = document.getElementsByClassName("delete-links")
+
+console.log(deleteLink)
+
+for (let link of deleteLink) {
+    link.addEventListener("click", async function (e) {
+        console.log("Delete link clicked");
+        if (e.target.className === "delete-links") {
+            e.preventDefault();
+
+            let linkId = e.target.dataset.id;
+            console.log("Deleting post with ID:", linkId);
+            let response = await fetch("https://blog-api-assignment.up.railway.app/posts/" + linkId, {
+                method: `DELETE`
+            });
+        }
+
+    })
+
+
 }
