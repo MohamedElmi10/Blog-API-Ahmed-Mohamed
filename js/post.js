91996eff97261881bdf7e834728664bc3d34963c
Ahmed Idiris <aamedidris1999@gmail.com>
2023-12-11 10:20:58 +0100

    json

--- js/post.js
+++ js/post.js
@@ -5,8 +5,14 @@ const postId = urlParams.get('id');
 async function fetchPostDetails() {
   try{
     const response = await fetch(`https://blog-api-assignment.up.railway.app/posts/${postId}`);
-
+    if (!response.ok) {
+      throw new Error(`API Request Failed: ${response.status} ${response.statusText}`);
   }
+  const post = await response.json();
+  }
+  // Här kan du fortsätta  Mohammed
+
+
   catch (error) {
 console.error('Error fetching post details:', error.message);
   }
